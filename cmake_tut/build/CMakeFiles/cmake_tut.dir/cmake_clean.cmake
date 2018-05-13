494b84cc1bf0f3d3381eeff207580fe7a8a29017
Nam <vqnam97@gmail.com>
2018-05-13 20:07:25 +0700

    cmak

---
+++ cmake_tut/build/CMakeFiles/cmake_tut.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_tut.dir/src/main.cpp.o"
+  "CMakeFiles/cmake_tut.dir/src/get_pass.cpp.o"
+  "cmake_tut.pdb"
+  "cmake_tut"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_tut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake_tut/include/get_pass.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
+
+#define ENOUGH_SIZE 256
+
+#define ECHO_ON 1
+#define ECHO_OFF 0
+#define BACK_SPACE 127
+
+char *get_pass (void);
